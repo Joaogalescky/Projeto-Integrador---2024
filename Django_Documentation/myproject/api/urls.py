3a2ef73da3bbee45172e4039564f66a530d65d47
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-08 13:54:00 -0300

    fix: urlpatterns

--- Django_Documentation/myproject/api/urls.py
+++ Django_Documentation/myproject/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ItemListCreate, ItemDetail
 
-urlpatters = [
-    path('item/', ItemListCreate.as_view(), name='item-list-create'),
+urlpatterns = [
+    path('items/', ItemListCreate.as_view(), name='item-list-create'),
     path('items/<int:pk>/', ItemDetail.as_view(), name='item-detail'),
 ]
