81ff18c4f156004b51df9bdb82992cb8780ef825
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-09 11:56:02 -0300

    refactor: models.py

--- Django_Documentation/myproject/api/models.py
+++ Django_Documentation/myproject/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
     
     def __str__(self):
-        return self.name
+        return self.nome
