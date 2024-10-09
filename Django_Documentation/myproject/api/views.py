2b088e4436b4f176fc54060d3fb58fa5160ac353
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-09 11:56:50 -0300

    refactor: views.py

--- Django_Documentation/myproject/api/views.py
+++ Django_Documentation/myproject/api/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Item
 from .serializers import ItemSerializer
 
 # Create your views here.
-class ItemListCreate(generics.ListCreateAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-    
-class ItemDetail(generics.RetrieveAPIView):
+class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
