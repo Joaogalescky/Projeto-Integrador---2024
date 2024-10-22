e3be188d425cd785f142a86941e4e7a5d93080cc
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-22 20:40:13 -0300

    feat: Importações

---
+++ Projeto/SCEAS/cam.py
@@ -0,0 +1,6 @@
+import cv2 # Visão computacional
+import numpy as np # Manipulação de arrays
+import pyzbar.pyzbar as pyz # Decodificar códigos de barras
+import urllib.request # Requisições HTTP
+
+
