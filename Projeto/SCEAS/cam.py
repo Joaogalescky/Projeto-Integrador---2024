fdb843c225eba0486f8ec1655a911c153262ee29
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-22 20:42:35 -0300

    feat: Configuração de vídeo

--- Projeto/SCEAS/cam.py
+++ Projeto/SCEAS/cam.py
@@ -3,4 +3,6 @@ import numpy as np # Manipulação de arrays
 import pyzbar.pyzbar as pyz # Decodificar códigos de barras
 import urllib.request # Requisições HTTP
 
-
+# Captura de vídeo
+#cap = cv2.VideoCapture(0) 
+font = cv2.FONT_HERSHEY_PLAIN # Fonte
