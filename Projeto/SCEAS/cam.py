055c45312428f41f62da06d483f5f1b46a86fdce
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-22 22:21:29 -0300

    feat: Loop de iteração do QR Code

--- Projeto/SCEAS/cam.py
+++ Projeto/SCEAS/cam.py
@@ -16,3 +16,14 @@ while True:
     img_resp = urllib.request.urlopen(url + 'cam-hi.jpg') # Requisição para a URL
     imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8) # Converte os dados
     frame = cv2.imdecode(imgnp, -1) # Decodifica o array para o OpenCV usar
+    decodedObjects = pyz.decode(frame) # Biblioteca para decodificar
+    for obj in decodedObjects: # Iteração para QR Code
+        pres=obj.data # Armazena o QR Code
+        if prev == pres:
+            pass
+        else: # Imprima os dados
+            print("Type:",obj.type)
+            print("Data: ",obj.data)
+            prev=pres # Armazene
+        cv2.putText(frame, str(obj.data), (50, 50), font, 2, (255, 0, 0), 3) # Adicione o texto na posição (50, 50)
+    cv2.imshow("live transmission", frame)
