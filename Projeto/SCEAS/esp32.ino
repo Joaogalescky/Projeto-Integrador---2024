515929f3eb88064713100c666841e62f101d7dbb
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-23 00:16:58 -0300

    feat: Configuração do Firebase

--- Projeto/SCEAS/esp32.ino
+++ Projeto/SCEAS/esp32.ino
@@ -8,3 +8,9 @@
 // Configurações de Wi-Fi
 const char* WIFI_SSID = "ALHN-BA05";
 const char* WIFI_PASS = "7740976294";
+
+// Configurações do Firebase
+#define API_KEY "AIzaSyAm4JouCBP2uqzbdgERpx_Jc8mSOEMooOU"
+#define USER_EMAIL "joaocvgalescky@gmail.com"
+#define USER_PASSWORD "sceas2024"
+#define DATABASE_URL "https://sceas-49731.firebaseio.com"
