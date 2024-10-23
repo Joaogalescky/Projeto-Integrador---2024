4a50c53412e2bb98529ba178fed6f279080d405c
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-22 21:57:02 -0300

    feat: URL e Janela

--- Projeto/SCEAS/cam.py
+++ Projeto/SCEAS/cam.py
@@ -3,9 +3,11 @@ import numpy as np # Manipulação de arrays
 import pyzbar.pyzbar as pyz # Decodificar códigos de barras
 import urllib.request # Requisições HTTP
 
-# Captura de vídeo
-#cap = cv2.VideoCapture(0) 
+#cap = cv2.VideoCapture(0) # Captura de vídeo 
 font = cv2.FONT_HERSHEY_PLAIN # Fonte
+
+url = 'http://192.168.1.109/' # URL de acesso
+cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE) # Criar janela
 
 prev = "" # Armazena o último QR Code lido
 pres = "" # Armazena o QR Code atual
