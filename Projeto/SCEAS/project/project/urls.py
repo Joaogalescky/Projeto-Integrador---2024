0309e3da617dbf4e67c7ddc65316904f648d8262
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-23 23:39:52 -0300

    feat: esquema e urlpatterns

--- Projeto/SCEAS/project/project/urls.py
+++ Projeto/SCEAS/project/project/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SCEAS - API",
+        default_version='v1',
+        description="Sistema de Controle de Entrada de Alunos e Servidores",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="joaocvgalescky@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Inclua suas URLs da API
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Interface do Swagger
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # Interface do ReDoc
 ]
